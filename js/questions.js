8987a95abeb8038fd25c98aa6f12afe7b805ed2d
Jose Sebastian Pinto Rondón <josesepint3@gmail.com>
2026-08-20 09:49:56 -0500

    feat: preguntas personalizadas

--- js/questions.js
+++ js/questions.js
@@ -4,7 +4,7 @@ const QUIZ_CONFIG = {
   questions: [
     {
       question: "¿Cuál es mi color favorito?",
-      options: ["Azul", "Verde", "Rojo", "Blanco"]
+      options: ["Azul", "Verde", "Rojo", "Negro"]
     },
     {
       question: "¿Pizza o tacos?",
@@ -12,35 +12,35 @@ const QUIZ_CONFIG = {
     },
     {
       question: "¿Soy de día o de noche?",
-      options: ["Mañana", "Tarde", "Noche", "Madrugada"]
+      options: ["Madrugador", "Tarde", "Nocturno", "Madrugada"]
     },
     {
       question: "¿Cuántas horas duermo?",
       options: ["Menos de 5", "5-7", "7-9", "Mas de 9"]
     },
     {
-      question: "¿Uso lentes?",
-      options: ["Si", "No"]
+      question: "¿Qué estudio?",
+      options: ["Sistemas/Software", "Electrónica", "Mecánica", "Otra ingeniería"]
     },
     {
       question: "¿Qué música escucho?",
-      options: ["Pop", "Rock", "Reggaeton", "Otra"]
-    },
-    {
-      question: "¿Cuál es mi comida favorita?",
-      options: ["Tacos", "Pizza", "Sushi", "Hamburguesa"]
-    },
-    {
-      question: "¿Soy organized o caótico?",
-      options: ["Organizado", "Caótico", "Un poco de ambos"]
+      options: ["Rock", "Pop", "Reggaeton", "Un poco de todo"]
     },
     {
       question: "¿Cuál es mi pasatiempo favorito?",
       options: ["Videojuegos", "Leer", "Deporte", "Musica"]
     },
     {
-      question: "¿Cuál es mi miedo mayor?",
-      options: ["Alturas", "Oscuridad", "Hablar en publico", "Otros"]
+      question: "¿Cómo describirías mi personalidad?",
+      options: ["Organizado", "Caótico", "Relajado", "Impulsivo"]
+    },
+    {
+      question: "¿Cómo aprendo mejor?",
+      options: ["Videos/Tutoriales", "Lectura", "Práctica", "Conversando"]
+    },
+    {
+      question: "¿Cuál es mi mayor miedo?",
+      options: ["Alturas", "Oscuridad", "La muerte", "Hablar en público"]
     }
   ]
 };
